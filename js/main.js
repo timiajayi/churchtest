225c7b47a7b48866ed019985cdb2a1739b015029
timiajayi <timiajayi2001@gmail.com>
2021-10-12 01:30:12 +0100

    update 1.0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+$(function () {
+	var includes = $('[data-include]')
+	$.each(includes, function () {
+	  var file = '' + $(this).data('include') + '.html'
+	  $(this).load(file)
+	})
+  })
+  
 ;(function () {
 	
 	'use strict';
